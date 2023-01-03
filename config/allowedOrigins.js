425824a7ca9d6c4a3e9bf20f65cbde08a8824e6d
hernan0066 <hernan0066@gmail.com>
2023-01-03 17:50:57 -0300

    Update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
     'https://dr-dashboard.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
+    'http://localhost:3010',
+    'https://www.distribuidora-ringo.com.ar',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ];
