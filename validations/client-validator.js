223b2738509df4be30710ad46e52ba6677fc8e9d
hernan0066 <hernan0066@gmail.com>
2023-01-20 11:53:18 -0300

    Change validation in cliente post

--- validations/client-validator.js
+++ validations/client-validator.js
@@ -10,15 +10,14 @@ const getClientValidator = [
   validateFields,
 ];
 const postClientValidator = [
-  validarJWT,
+ 
   check("user", "No es un id de Mongo válido").isMongoId(),
   check("user").custom(existUserById),
   check("clientCategory", "No es un id de Mongo válido").isMongoId(),
   check("clientCategory").custom(existClientCategoryById),
   check("clientType", "No es un id de Mongo válido").isMongoId(),
   check("clientType").custom(existClientTypeById),
-  check("cuit", "El cuit es obligatorio").not().isEmpty(),
-  check("contactMeans", "Los medios de contacto son obligatorios").not().isEmpty(),
+ 
   validateFields,
 ];
 const putClientValidator = [
