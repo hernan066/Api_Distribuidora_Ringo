ba3cf8f3fbd9390ecb44bd81869496cd6f72feba
hernan0066 <hernan0066@gmail.com>
2023-02-03 19:05:45 -0300

    Update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -7,6 +7,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:3015',
+    'https://dr-distribution-app.netlify.app'
 ];
 
 module.exports = allowedOrigins;
