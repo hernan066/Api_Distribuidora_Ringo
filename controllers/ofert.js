338466978442683d42281481b5b03c84de2ed142
hernan0066 <hernan0066@gmail.com>
2023-03-10 18:57:07 -0300

    Update oferts controllers

--- controllers/ofert.js
+++ controllers/ofert.js
@@ -19,6 +19,7 @@ const getOferts = async (req, res = response) => {
           "brand",
           "category",
           "type",
+          "stock"
         ]),
     ]);
 
@@ -50,6 +51,7 @@ const getOfert = async (req, res = response) => {
       "brand",
       "category",
       "type",
+      "stock"
     ]);
 
     res.status(200).json({
