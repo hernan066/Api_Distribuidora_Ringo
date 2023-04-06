7fd000f04cf68a9d488b9d80dbc51dfa38f45edc
hernan0066 <hernan0066@gmail.com>
2023-04-06 12:43:46 -0300

    Add reportTotalOrdersByMonth controller

--- controllers/report.js
+++ controllers/report.js
@@ -85,6 +85,88 @@ const reportTotalOrdersByMonth = async (req, res = response) => {
     });
   }
 };
+/* const reportTotalOrdersByMonth = async (req, res = response) => {
+  try {
+    const report = await Order.aggregate([
+      {
+        $match: {
+          state: true,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            month: {
+              $month: "$deliveryDate",
+            },
+            year: {
+              $year: "$deliveryDate",
+            },
+          },
+          totalSales: {
+            $sum: "$total",
+          },
+          totalCash: {
+            $sum: "$payment.cash",
+          },
+          totalTransfer: {
+            $sum: "$payment.transfer",
+          },
+          totalDebt: {
+            $sum: "$payment.debt",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+
+          month: {
+            $toString: "$_id.month",
+          },
+          year: {
+            $toString: "$_id.year",
+          },
+          totalSales: 1,
+          totalCash: 1,
+          totalTransfer: 1,
+          totalDebt: 1,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: {
+            $concat: ["$month", "-", "$year"],
+          },
+          totalSales: 1,
+          totalCash: 1,
+          totalTransfer: 1,
+          totalDebt: 1,
+        },
+      },
+      {
+        $sort: {
+          date: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      ok: true,
+      status: 200,
+      data: {
+        report,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      status: 500,
+      msg: error.message,
+    });
+  }
+}; */
 const reportTotalOrdersByDay = async (req, res = response) => {
   try {
     const report = await Order.aggregate([
